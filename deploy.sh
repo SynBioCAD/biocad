7c239b36827dc0af2de65eac66aecfc3c5c4a364
James Alastair McLaughlin <j.a.mclaughlin@ncl.ac.uk>
2018-10-04 11:18:47 +0100

    add commit/push to deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,12 +4,16 @@ rm -rf deploy
 
 git clone git@github.com:ngbiocad/biocad.git -b gh-pages deploy
 
-webpack
+node ./node_modules/webpack/bin/webpack.js 
 
 cp -r dist/* deploy/
 
 mkdir deploy/css
 lessc less/biocad.less --include-path=node_modules/jfw/less > ./deploy/css/biocad.css
+
+cd deploy
+git commit -a -m "deploy"
+git push origin gh-pages
 
 
 
