0b948343c5911af8b85ac3c7fc2336b1b2ecfe19
James Alastair McLaughlin <james@mclgh.net>
2018-11-09 18:55:21 +0000

    Separate browser and node webpack configs

--- dev-server.js
+++ dev-server.js
@@ -12,7 +12,7 @@
 
 var MemoryFS = require("memory-fs")
 var webpack = require('webpack')
-var config = require('./webpack.config')
+var config = require('./webpack_browser.config')
 var compiler = webpack(config)
 var express = require('express')
 var mime = require('mime')
