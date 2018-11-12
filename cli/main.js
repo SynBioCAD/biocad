4ec595002cfeeead47f0a7960b42e8af66a87c0f
James Alastair McLaughlin <james@mclgh.net>
2018-11-12 17:38:44 +0100

    deploy

--- cli/main.js
+++ cli/main.js
@@ -12,23 +12,34 @@ const Layout_1 = require("biocad/cad/Layout");
 const ImageRenderer_1 = require("biocad/cad/ImageRenderer");
 const sbolgraph_1 = require("sbolgraph");
 const geom_1 = require("jfw/geom");
-let fs = require('fs');
+const fs = require("fs");
 main();
 function main() {
     return __awaiter(this, void 0, void 0, function* () {
-        for (let file of fs.readdirSync('testfiles')) {
-            if (!file.endsWith('.xml') || file.endsWith('_sbolx.xml'))
-                continue;
-            let path = 'testfiles/' + file;
-            let graph = yield sbolgraph_1.SBOLXGraph.loadString(fs.readFileSync(path) + '', 'application/rdf+xml');
-            fs.writeFileSync('testfiles/' + file + '_sbolx.xml', graph.serializeXML());
-            let layout = new Layout_1.default(graph);
-            layout.syncAllDepictions(5);
-            layout.configurate([]);
-            layout.size = layout.getBoundingSize().add(geom_1.Vec2.fromXY(1, 1));
-            let renderer = new ImageRenderer_1.default(layout);
-            let svg = renderer.renderToSVGString();
-            fs.writeFileSync('testfiles/' + file + '.svg', svg);
+        let f = process.argv.slice(2).join(' ');
+        if (f) {
+            yield doFile(f);
+        }
+        else {
+            for (let file of fs.readdirSync('testfiles')) {
+                if (!file.endsWith('.xml') || file.endsWith('_sbolx.xml'))
+                    continue;
+                let path = 'testfiles/' + file;
+                yield doFile(path);
+            }
+        }
+        function doFile(path) {
+            return __awaiter(this, void 0, void 0, function* () {
+                let graph = yield sbolgraph_1.SBOLXGraph.loadString(fs.readFileSync(path) + '', 'application/rdf+xml');
+                //fs.writeFileSync('testfiles/' + file + '_sbolx.xml', graph.serializeXML())
+                let layout = new Layout_1.default(graph);
+                layout.syncAllDepictions(5);
+                layout.configurate([]);
+                layout.size = layout.getBoundingSize().add(geom_1.Vec2.fromXY(1, 1));
+                let renderer = new ImageRenderer_1.default(layout);
+                let svg = renderer.renderToSVGString();
+                fs.writeFileSync(path + '.svg', svg);
+            });
         }
     });
 }
